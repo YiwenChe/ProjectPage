110cfb4e6788d6135529e8e25d2b987e50f9b238
Che <yche@kpf.com>
2021-04-22 12:28:53 -0400

    updated

--- script.js
+++ script.js
@@ -1,3 +1,48 @@
-fetch('http://example.com/movies.json')
-  .then(response => response.json())
-  .then(data => console.log(data));
+// variables
+const container = document.getElementById('container')
+const targetProject_id = "2635-2-000"
+const url = "https://raw.githubusercontent.com/YiwenChe/ProjectPage/main/sample.json"
+
+function createNode(element, classname) {
+    const newNode = document.createElement(element)
+    newNode.classList.add(classname)
+    return newNode
+}
+
+function append(parent, el) {
+    return parent.appendChild(el)
+}
+
+// fetch json
+fetch(url)
+    .then(response => response.json())
+    .then(data => {
+        console.log(data)
+        data.forEach(project => {
+            if (project["Id"] === targetProject_id) {
+                project["Cards"].map(card => {
+                    let cardWrapper = createNode('div', 'cardWrapper')
+
+                    let title = createNode('h3', 'title')
+                    title.innerHTML = card['Title']
+
+                    let image = createNode('img', 'image')
+                    image.src = card['Image']
+
+                    let link = createNode('a', 'link')
+                    link.href = card['Link']
+
+                    let des = createNode('p', 'des')
+
+                    append(cardWrapper, link)
+                    // append(link, title)
+                    // append(link, image)
+                    // append(link, link)
+                    // append(link, des)
+
+
+                    console.log(cardWrapper)
+                })
+            }
+        })
+    })
